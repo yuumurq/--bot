71e9891fac46dfca6821ac0ef6d6417736408ed3
yumurq <2005yu0615to@gmail.com>
2023-03-09 23:51:51 +0900

    ignore

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import discord.app_commands
 import os
 import dotenv
 # This example requires the 'message_content' intent.
@@ -5,23 +6,25 @@ import dotenv
 import discord
 import random
 dotenv.load_dotenv()
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
 
-    async def on_message(self, message):
-        kondatelist = ["デミグラスソースハンバーグ","カレイの煮つけ","肉じゃが","トンカツ","カツオのたたき","生姜焼き","エビグラタン","麻婆豆腐","あじの塩焼き","唐揚げ","ピーマンの肉詰め","ポークソテー","鳥の照り焼き","エビチリ","豚丼","カレイの煮つけ","肉じゃが","トンカツ","カツオのたたき","生姜焼き","エビグラタン","麻婆豆腐","あじの塩焼き","唐揚げ","ピーマンの肉詰め","ポークソテー","鳥の照り焼き","エビチリ","豚丼"]
-
-        if message.content == '/kondate':
-            await message.channel.send(random.choice(kondatelist))
-        print(f'Message from {message.author}: {message.content}')
-    
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
+@tree.command(
+    name="kondate",
+    description="適当に献立を決めてくれます。"
+)
+async def hoge(ctx:discord.Interaction):
+    kondatelist = ["デミグラスソースハンバーグ","カレイの煮つけ","肉じゃが","トンカツ","カツオのたたき","生姜焼き","エビグラタン","麻婆豆腐","あじの塩焼き","唐揚げ","ピーマンの肉詰め","ポークソテー","鳥の照り焼き","エビチリ","豚丼","カレイの煮つけ","肉じゃが","トンカツ","カツオのたたき","生姜焼き","エビグラタン","麻婆豆腐","あじの塩焼き","唐揚げ","ピーマンの肉詰め","ポークソテー","鳥の照り焼き","エビチリ","豚丼"]
+    await ctx.response.send_message(random.choice(kondatelist))
+@client.event
+async def on_ready():
+    await tree.sync()
 client.run(os.getenv("BOT_TOKEN"))
+
 
 
 
